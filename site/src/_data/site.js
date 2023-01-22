b89481a403a230e7c14115f33aba694e487d8811
dwkns <dwkns@me.com>
2023-01-22 21:34:49 +0000

    Add metadata

--- site/src/_data/site.js
+++ site/src/_data/site.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 let site = {
   name: "comapny name",
   locale: "en_GB",
   legalName: "comapny name ltd",
-  url: "https://someurl.com",
+  url: "https://dwkns-house-map.netlify.app",
   logo: "https://someurl.com/images/logo.png",
   foundingDate: "2019",
   privacyEmail: "default_privacy@esomeurl.com",
@@ -13,42 +13,42 @@ let site = {
     streetAddress: "default_streetAddress",
     addressRegion: "default_addressRegion",
     postalCode: "default_postalCode",
-    addressCountry: "default_addressCountry"
+    addressCountry: "default_addressCountry",
   },
   theme: {
-   colour: '#93C5FD'
+    colour: "#93C5FD",
   },
   twitterURL: "https://twitter.com/yourTwitter",
   linkedInURL: "https://www.linkedin.com/company/yourCompany/",
   author: {
     twitterHandle: "@default_twitter_handle",
-    name: "dazza"
+    name: "dazza",
   },
   metadata: {
-    default_og_image: '/images/default_og_image.png',
-    default_og_type: 'article'
+    default_og_image: "/images/default_og_image.png",
+    default_og_type: "article",
   },
   development: {
-    domain: 'localhost',
-    baseURL: 'https://localhost:8080',
+    domain: "localhost",
+    baseURL: "https://localhost:8080",
     debug: true,
     minifyCSS: false,
   },
   staging: {
-    domain: 'staging-tiny-start.netlify.app',
-    baseURL: 'https://staging-tiny-start.netlify.app',
+    domain: "staging-tiny-start.netlify.app",
+    baseURL: "https://staging-tiny-start.netlify.app",
     debug: true,
     minifyCSS: true,
   },
   production: {
-    domain: 'tiny-start.netlify.app',
-    baseURL: 'https://tiny-start.netlify.app',
+    domain: "tiny-start.netlify.app",
+    baseURL: "https://tiny-start.netlify.app",
     debug: false,
     minifyCSS: true,
   },
   currentYear: new Date().getFullYear(),
   currentDate: new Date(),
   version: Math.random().toString(36).substr(2, 8),
-  currentEnv: process.env.CURRENT_ENV
-}
-module.exports = site
+  currentEnv: process.env.CURRENT_ENV,
+};
+module.exports = site;
